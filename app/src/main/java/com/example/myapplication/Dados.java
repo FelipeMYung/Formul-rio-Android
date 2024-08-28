a3883505b87923fb5219770dfe542999fd6f8832
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-08-28 09:26:45 -0300

    funcionalidade pronta

--- app/src/main/java/com/example/myapplication/Dados.java
+++ app/src/main/java/com/example/myapplication/Dados.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Dados extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class Dados extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados);
+
+        TextView txtNome = findViewById(R.id.telaNome);
+        TextView txtIdade = findViewById(R.id.telaIdade);
+        TextView txtCPF = findViewById(R.id.telaCPF);
+        TextView txtEmail = findViewById(R.id.telaEmail);
+        Intent intent = getIntent();
+        String MsgNome = intent.getStringExtra("nome");
+        String MsgIdade = intent.getStringExtra("idade");
+        String MsgCPF = intent.getStringExtra("CPF");
+        String MsgEmail = intent.getStringExtra("Email");
+
+        txtNome.setText(MsgNome);
+        txtIdade.setText(MsgIdade);
+        txtCPF.setText(MsgCPF);
+        txtEmail.setText(MsgEmail);
     }
 }
